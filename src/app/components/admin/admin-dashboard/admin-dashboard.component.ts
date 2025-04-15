d94dfb1b38bf23df07c213a2c4ff0e786a13ca90
Luza00Ros <lucianarosati6@gmail.com>
2025-04-15 22:32:14 +0200

    Aggiunto JSDoc per componente "admin-dshbord.component.ts" percorso relativo: admin-accont >> admin-dashbord

--- src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin/admin-dashboard/admin-dashboard.component.ts
@@ -14,6 +14,9 @@ enum VisteUtente {
   BILLING = 'billing'
 }
 
+/**
+ * Componente per la dashbord dell'amministratore
+ */
 @Component({
   selector: 'app-admin-dashboard',
   imports: [NgIcon, AdminAccountComponent, AdminBillingComponent],
@@ -23,38 +26,71 @@ enum VisteUtente {
 })
 export class AdminDashboardComponent {
 
+  /** Enum delle viste disponibili per l'utente */
   readonly visteUtente = VisteUtente;
+  /** Viste disponibili visibili all'utente */
   readonly visteDisponibili = Object.values(VisteUtente);
-
+  /** Vista attualmente visibile sulla dashbord */
   public vistaCorrente: VisteUtente = VisteUtente.ACCOUNT;
+  /** Utente attualmente autenticato o selzionato */
   public utenteCorrente: Utente | null = null;
-
+  /** Componente che indica una fase di caricamento */
   public loading: boolean = true;
 
   public constructor(private authService: AuthService, private utenteService: UtenteService, private router: Router) { }
 
+  /**
+   * Hook di inizializzazione del componente
+   * Carica l'utente corrente 
+   */
   public ngOnInit(): void {
     this.caricaUtenti();
   }
 
+  /** L'utente corrente o autentico 
+   * esce dalla sessione di lavoro 
+  */
   public async logout(): Promise<void> {
     await this.authService.logout();
   }
 
+  /**
+   * Cambia la vista attualmente selezionata 
+   * nella dashbord
+   * @param vista Vista selezionata dall'utente
+   */
   public cambiaVistaCorrente(vista: VisteUtente): void {
     if (this.isVistaValida(vista)) {
       this.vistaCorrente = vista;
     }
   }
 
+  /**
+   * Verifica se la vista selezionata è 
+   * disponibile e valida per l'utente
+   * @param vista Vista selezionata dall'utente
+   * @returns true se la vista selezionata è compresa nelle viste dispobili
+   */
   public isVistaValida(vista: VisteUtente): boolean {
     return this.visteDisponibili.includes(vista);
   }
 
+  /**
+   * Verfica se la vista selezionata dall'utente è
+   * la vista corrente attiva
+   * @param vista Vista selezionata dall'utente
+   * @returns true se la vista selezionata è la stessa della vista corrente
+   */
   public isVistaCorrente(vista: VisteUtente): boolean {
     return this.vistaCorrente === vista;
   }
 
+  /**
+   * Carica l'utente autenticato 
+   * Se utente esistente completa caricamento
+   * Se inesistente effettua logout e reinderizza 
+   * alla pagina di login
+   */
   private async caricaUtenti(): Promise<void> {
     try {
       const utente = this.authService.getUtenteCorrente();
